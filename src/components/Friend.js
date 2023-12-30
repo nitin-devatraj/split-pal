f9b06c713cfd5e1fc8c8a88e61f481d2a3633623
nitin devatraj <nitindevatraj@protonmail.com>
2023-12-31 02:00:14 +0530

    add initial Friend & FriendsList component

---
+++ src/components/Friend.js
@@ -0,0 +1,20 @@
+export default function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          you owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>you and {friend.name} are even</p>}
+      <button className="button">select</button>
+    </li>
+  );
+}
---
+++ src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import Friend from "./Friend";
+
+export default function FriendsList({ initialFriends }) {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
